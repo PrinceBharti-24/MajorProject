3877eff2bd551d2a59897fb10112014485ebb5eb
Prince Bharti <Prince.pbharti667@gmail.com>
2023-07-19 12:22:09 +0530

    add assets and views

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayout = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+app.use(expressLayout);
+
+// set up the layout for script and style so that it will automatically set in the head and body respectively
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 // use express router
 
 app.use('/', require('./routes'))
-
 // set up the view engine
 
 app.set('view engine', 'ejs');
