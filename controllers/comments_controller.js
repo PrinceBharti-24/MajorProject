019719acbac60df417f6113c10a1e4fab4feec17
Prince Bharti <Prince.pbharti667@gmail.com>
2023-07-25 14:29:50 +0530

    add delete comment functionality

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -24,3 +24,23 @@ module.exports.create = async function (req, res) {
     console.log("error in creating a comment", err);
   }
 };
+
+module.exports.destroy = async function(req, res){
+  try{
+    // Finding the Id of the Comment clicked using params.id and delete it and store the id in comment
+    const comment = await Comment.findByIdAndDelete(req.params.id);
+
+    if(comment.user == req.user.id){
+        let postId = comment.post;
+        // Store the PostId to delete the comment from the post
+        // for this use update method and pull the comment from it
+        // This pull method is provided by mongoose to delete the comments from the comment array of the post 
+        await Post.findByIdAndUpdate(postId, {$pull : {comments: req.params.id}});
+        return res.redirect('back')
+        console.log('getting in this if block')
+    }
+  }
+  catch(err){
+    console.log('error in deleting the comment', err);
+  }
+}
